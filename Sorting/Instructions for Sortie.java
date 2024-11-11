cdf87b1325b8ec1af63b21f93dc603bf624c4ac0
Pradip Sapkota <141882656+pradip676@users.noreply.github.com>
2024-11-11 16:12:41 -0600

    Create Instructions for Sortie.java

---
+++ Sorting/Instructions for Sortie.java
@@ -0,0 +1,33 @@
+Please demo the program to the instructor during lab or upload a single .java file to Ecampus.
+
+
+Create a program named sort.java that reads in primes1.txt.  Sort these numbers using bubble, selection, merge, and radix sorts.  Each sort should list the elapsed time, iterations, and print the first 10 and last 10 numbers in the sorted array.
+
+
+Example output
+This program compares the bubble, selection, merge, and radix sorts.
+The data set is 100000 prime numbers unsorted
+
+1. Bubble Sort
+Seconds to sort = ???
+Number of iterations = ???
+First 10 - 2 3 5 7 11 17 23 29 31 37
+Last 10 - 1299553 1299583 1299601 1299631 1299637 1299647 1299653 1299673 1299689 1299709
+
+2. Selection Sort
+Seconds to sort = ???
+Number of iterations = ???
+First 10 - 2 3 5 7 11 17 23 29 31 37
+Last 10 - 1299553 1299583 1299601 1299631 1299637 1299647 1299653 1299673 1299689 1299709
+
+3. Merge Sort
+Seconds to sort = ???
+Number of iterations = ???
+First 10 - 2 3 5 7 11 17 23 29 31 37
+Last 10 - 1299553 1299583 1299601 1299631 1299637 1299647 1299653 1299673 1299689 1299709 
+
+4. Radix Sort
+Seconds to sort = ???
+Number of iterations = ???
+First 10 - 2 3 5 7 11 17 23 29 31 37
+Last 10 - 1299553 1299583 1299601 1299631 1299637 1299647 1299653 1299673 1299689 1299709 
