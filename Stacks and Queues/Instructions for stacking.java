05981dd49d5423eb81d2e47b56fab6ca2591fcb7
Pradip Sapkota <141882656+pradip676@users.noreply.github.com>
2024-11-20 13:28:29 -0600

    Create Instructions for stacking.java

---
+++ Stacks and Queues/Instructions for stacking.java
@@ -0,0 +1,23 @@
+Please demo the programs to the instructor during open lab or upload the .java files to Ecampus.
+
+
+1.  Create a program named parentheses.java that reads in a string of various parentheses, brackets, and braces from standard input.  Write a method that checks whether or not the parentheses, brackets, and braces are balanced.
+
+
+Example output
+This program checks if the parentheses, brackets, and braces are balanced.
+Please enter a string:
+(({}[()]{[]}))
+This string is balanced.
+
+This program checks if the parentheses, brackets, and braces are balanced.
+Please enter a string:
+{[])[()]}
+This string is not balanced.
+ 
+
+Complete one of the two programs:
+
+Option 2a.  Create a program named equation.java that uses two stacks (Value and Operator) to evaluate a simple equation.  It should let the user enter an equation that can include addition, subtraction, multiplication, division, and parentheses.
+
+Example equation:  (2+3)*(8-4)*(5-3)
